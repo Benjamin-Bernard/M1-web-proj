b409436088a7012064dea0213dca501c3902cb6a
Stan97-2 <stan.dellon972@gmail.com>
2022-10-21 16:35:44 +0200

    Adresses suggestions added / lon and lat props

--- ViteMaRame/src/components/RouteCalculation.vue
+++ ViteMaRame/src/components/RouteCalculation.vue
@@ -1,11 +1,36 @@
 <template>
-    <div>
+    <div class="route-calculation" @click="closeSuggestions">
         <h1>Calculer mon itinéraire</h1>
         <div class="form">
             <form>
                 <p id="inavalid-data-msg" v-if="invalidData">Le mot de passe doit être composée d'au moins 8 caractères</p>
-                <input class="wide-input" type="text" placeholder="Choisissez un point de départ" v-model="form.depart" required>
-                <input class="wide-input" type="text" placeholder="Choisissez une destination" v-model="form.destination" required>
+                <input 
+                    class="wide-input" 
+                    type="text" 
+                    placeholder="Choisissez un point de départ" 
+                    v-model="form.depart" @keyup="fetchsuggestions(form.depart)" 
+                    @click="openSuggestionsDepart=true" 
+                    required>
+                <div  class="suggestions" v-if="form.depart.length>0 && openSuggestionsDepart">
+                    <ul>
+                        <li v-for="adress in suggestions" :key="adress.adress_line1" @click="selectDepart(adress)">
+                            {{ adress.properties.formatted}}
+                        </li>
+                    </ul>
+                </div>
+                <input 
+                    class="wide-input" 
+                    type="text" placeholder="Choisissez une destination" 
+                    v-model="form.destination" @keyup="fetchsuggestions(form.destination)" 
+                    @click="openSuggestionsDestiantion=true" 
+                    required>
+                <div  class="suggestions" v-if="form.destination.length>0 && openSuggestionsDestiantion">
+                    <ul>
+                        <li v-for="adress in suggestions" :key="adress.adress_line1" @click="selectDestination(adress)">
+                            {{ adress.properties.formatted}}
+                        </li>
+                    </ul>
+                </div>
                 <p>Options</p>
                 <select class="medium-input" name="time" id="time" v-model="form.time" :change="getTime()">
                     <option value="leaveNow">Partir maintenant</option>
@@ -21,9 +46,8 @@
                     <option value="tramway">Tramway</option>
                 </select>
                 <div class="btn-container">
-                    <button @click="searchAdresses">Calculer</button>
+                    <button>Calculer</button>
                 </div>
-                <p>{{ form.depart }}</p>
             </form>
         </div>
     </div>
@@ -35,56 +59,65 @@ export default {
   name: "RouteCalculation",
   data() {
     return {
+        openSuggestionsDepart: true,
+        openSuggestionsDestiantion: true,
+        suggestions: [],
         invalidData: false,
         leaveNow: true,
         form: {
             depart: "",
             destination: "",
             time: "",
-        }
+        },
+        adresses: {
+           depart: {},
+           destination: {}
+        }   
+        
     }
   },
   methods: {
     getTime() {
         this.leaveNow = (this.form.time === "leaveNow")
     }, 
-    // C'est juste un test xD
-    checkData() {
-        if (this.form.depart === "marseille") {
-            return true
-        }
-        else {
-            return false
-        }
-    }
-   
-    },
-    computed: {
-      searchAdresses() {
-      const URL = "https://api-adresse.data.gouv.fr/search/?q="
-      fetch(URL + this.form.depart + "&limit=5")
-        .then((res) => res.json())
-        .then((data) => {
-          const adresses = []
-          for (let i = 0; i < data.features.length; i++) {
-              const adress = {
-                  name: data.features[i].properties.name,
-                  postcode: data.features[i].properties.postcode,
-                  city: data.features[i].properties.city,
-                  coordinates: {
-                      lon: data.features[i].geometry.coordinates[0],
-                      lat: data.features[i].geometry.coordinates[1]
-                  }
-              }
-              adresses.push(adress) 
-              console.log(adresses)  
-              return adresses 
-          }
-        })
+    fetchsuggestions(adress) {
 
+        const url = "https://api.geoapify.com/v1/geocode/autocomplete?text="
+        setTimeout(() => {
+            if (this.form.depart.length != 0) {
+            var requestOptions = {
+            method: 'GET',
+        };
+
+        fetch(url + adress +"&lang=fr&limit=3&filter=countrycode:fr&apiKey=84f66adf152446288ac50036427af219", requestOptions)
+            .then(res => res.json())
+            .then(data => this.suggestions = data.features)
+            .catch(error => console.log('error', error));
+        }
+        }, 100)
+        
+    },
+    selectDepart(adress) {
+            this.openSuggestionsDepart = false
+            this.form.depart = adress.properties.formatted
+            this.adresses.depart = adress
+            this.suggestions.length = 0
+    },
+    selectDestination(adress) {
+            this.openSuggestionsDestiantion = false
+            this.form.destination = adress.properties.formatted
+            this.adresses.destination = adress
+            this.suggestions.length = 0
+    },
+    closeSuggestions() {
+
+    },
+    sendAdresses() {
+        this.$emit('sendAdresses', this.adresses)
     }
-    }
-  }
+    }  
+
+}
 </script>
 
 <style scoped>
@@ -95,6 +128,7 @@ h1 {
 }
 
 .form { 
+    position: relative;
     display: flex;
     flex-direction: column;
     background-color: #fff;
@@ -170,4 +204,40 @@ button:hover {
   background-color: rgb(34, 50, 128);
   transition: background-color 0.5s linear;
 }
+
+ul {
+    list-style-type: none;
+    padding: 3%;
+    border-radius: 4px;
+    height: 100%;
+    background-color: rgb(34, 50, 128);
+}
+
+li:hover {
+    cursor: pointer;
+    background-color: white;
+    color: rgb(34, 50, 128);
+    transition: background-color 0.5s linear;
+}
+
+li {
+    color: white;
+    border-bottom: 1px solid white;
+   
+    width: 99%;
+    border-radius: 4px;
+    margin: 1px;
+    height: 50px;
+    padding: 4px;
+}
+
+.suggestions {
+    position: absolute;
+    width: 100%;
+    height: 56%;
+    z-index: 100;
+    border-radius: 4px;
+}
+
+
 </style>
